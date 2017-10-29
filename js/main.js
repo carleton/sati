cc91860b684984d72a1c87afdba99a2ddff96229
Josh Pitkofsky <pitkofskyj@gmail.com>
2017-10-29 18:44:48 -0400

    almost done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var apps = "Meditation Apps"
 var three_five_seven_breathing_description = "4-7-8 Breathing"
 var body_scan_description = "Body Scan"
 var breathing_meditation_description = "Breathing Meditation"
-var senses  = "Mindfulness Activities"
+var senses = "Mindfulness Activities"
 var NYT_mindful_eating = "Mindful Eating"
 var walking = "Walking Meditation"
 var tea = "Cup of Tea"
@@ -15,7 +15,7 @@ var calm_sitting = "CALM Sitting Loving Kindness"
 var calm_walking_kindness = "CALM Walking Loving Kindness"
 var youtube_1 = "Relaxation Video"
 var youtube_2 = "Meditation to Reduce Anxiety"
-var youtube_3  = "Restful Sleep"
+var youtube_3 = "Restful Sleep"
 var tara_vipassana = "Tara Brach Vipassana"
 var tara_brief = "Tara Brach Brief"
 var tara_calm = "Tara Brach Calming"
@@ -24,30 +24,30 @@ var loving_kindness_text = "Loving-Kindness"
 var body_sound_text = "Body and Sound"
 
 var techniques = {
-apps : 1,
-three_five_seven_breathing_description : 1,
-body_scan_description : 1,
-breathing_meditation_description : 1,
-senses  : 1,
-NYT_mindful_eating  : 1,
-walking : 1,
-tea : 1,
-nyt_scan: 1,
-calm_breath : 1,
-calm_scan : 1,
-calm_muscle : 1,
-calm_walking : 1,
-calm_sitting : 1,
-calm_walking_kindness : 1,
-youtube_1 : 1,
-youtube_2 : 1,
-youtube_3  : 1,
-tara_vipassana : 1,
-tara_brief: 1,
-tara_calm : 1,
-self_compassion_break : 1,
-loving_kindness_text : 1,
-body_sound_text : 1
+    apps: 1,
+    three_five_seven_breathing_description: 1,
+    body_scan_description: 1,
+    breathing_meditation_description: 1,
+    senses: 1,
+    NYT_mindful_eating: 1,
+    walking: 1,
+    tea: 1,
+    nyt_scan: 1,
+    calm_breath: 1,
+    calm_scan: 1,
+    calm_muscle: 1,
+    calm_walking: 1,
+    calm_sitting: 1,
+    calm_walking_kindness: 1,
+    youtube_1: 1,
+    youtube_2: 1,
+    youtube_3: 1,
+    tara_vipassana: 1,
+    tara_brief: 1,
+    tara_calm: 1,
+    self_compassion_break: 1,
+    loving_kindness_text: 1,
+    body_sound_text: 1
 }
 sortList();
 
@@ -55,26 +55,26 @@ sortList();
 function sortList() {
     var sortable = [];
     for (var technique in techniques)
-        if(technique[0]!= 0){
-        sortable.push([technique, techniques[technique]]);
+        if (technique[0] != 0) {
+            sortable.push([technique, techniques[technique]]);
         }
     sortable.sort(function(a, b) {
         return a[1] - b[1]
     })
     var html = "";
     for (var i = 0; i < sortable.length; i++) {
-        html += "<li><a href=\"#\" class=\"ui-btn ui-btn-icon-right ui-icon-carat-r\">" + eval(sortable[i][0]) + "</a></li>";
+        html += "<li><a href=#" + sortable[i][0] + " class=\"ui-btn ui-btn-icon-right ui-icon-carat-r\">" + eval(sortable[i][0]) + "</a></li>";
     }
     document.getElementById("medlist").innerHTML = html;
 }
 
 function sensoryOverload() {
-    /*	Make meditation specific changes here
-    	Addition means worse choice for helping student
-    	Subtraction means better choice for helping student */
+    /*  Make meditation specific changes here
+        Addition means worse choice for helping student
+        Subtraction means better choice for helping student */
     var checkedValue = document.getElementById('sensoryOverload').checked;
     if (checkedValue) {
-    	techniques.three_five_seven_breathing_description = 0;
+        techniques.three_five_seven_breathing_description = 0;
         techniques.body_scan_description = 0;
         techniques.calm_scan = 0;
         sortList();
@@ -258,6 +258,7 @@ function practice() {
         sortList();
     }
 }
+
 function public() {
     /*  Make meditation specific changes here
         Addition means worse choice for helping student
@@ -428,6 +429,32 @@ function wifi() {
     }
 }
 
-
-
-
+function time() {
+    /*  Make meditation specific changes here
+        Addition means worse choice for helping student
+        Subtraction means better choice for helping student */
+    var checkedValue = document.getElementById('time').checked;
+    if (checkedValue) {
+        techniques.three_five_seven_breathing_description = 0;
+        techniques.body_scan_description = 0;
+        techniques.senses = 0;
+        techniques.tara_brief = 0;
+        techniques.calm_breath = 2;
+        techniques.calm_scan = 2;
+        techniques.calm_walking = 2;
+        techniques.youtube_1 = 2;
+        techniques.tara_vipassana = 2;
+        sortList();
+    } else {
+        techniques.three_five_seven_breathing_description = 1;
+        techniques.body_scan_description = 1;
+        techniques.senses = 1;
+        techniques.tara_brief = 1;
+        techniques.calm_breath = 1
+        techniques.calm_scan = 1;
+        techniques.calm_walking = 1
+        techniques.youtube_1 = 1;
+        techniques.tara_vipassana = 1
+        sortList();
+    }
+}
